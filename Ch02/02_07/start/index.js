7f2cf68ed41cfdd3dfadafd7518b555e864abe25
fanweikung <fan-wei.kung@unicodeconsulting.com>
2023-06-28 06:49:39 -0700

    Used PersonBuilder class to create instances

--- Ch02/02_07/start/index.js
+++ Ch02/02_07/start/index.js
@@ -1,13 +1,13 @@
-var Person = require("./Person");
+var PersonBuilder = require("./PersonBuilder");
 
 // Employees
-var sue = new Person("Sue", true, true, 60);
-var bill = new Person("Bill", true, false, 20);
-var phil = new Person("Phil", true, false);
+var sue = new PersonBuilder("sue").makeEmployee().makeManager(60).build();
+var bill = new PersonBuilder("Bill").makeEmployee().makePartTime.build();
+var phil = new PersonBuilder("Phil").makeEmployee().build();
 
 // Shoppers
-var charles = new Person("Charles", false, false, 0, 500, [
-  "jeans",
-  "sunglasses",
-]);
-var tabbitha = new Person("Tabbitha", false, false, 0, 1000);
+var charles = new PersonBuilder("charles")
+  .withMoney(500)
+  .withList(["jeans", "sunglasses"])
+  .build();
+var tabbitha = new PersonBuilder("Tabbitha").withMoney(1000).build();
