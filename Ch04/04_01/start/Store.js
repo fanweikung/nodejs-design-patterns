fe13d683dc91c45cd6a8c152c26ba7bfa4d6773d
fanweikung <fan-wei.kung@unicodeconsulting.com>
2023-07-01 07:16:30 -0700

    Updated Store to import Storage class

--- Ch04/04_01/start/Store.js
+++ Ch04/04_01/start/Store.js
@@ -1,12 +1,23 @@
+var Storage = require("./Storage");
+
 class Store {
   constructor(name, inventory = []) {
     this.name = name;
-    this.inventory = inventory;
+
+    var floor = new Storage("store floor", inventory.floor);
+    var backroom = new Storage("store backroom", inventory.backroom);
+    var localStore = new Storage("local store", inventory.localStore, 1);
+    var warehouse = new Storage("warehouse", inventory.warehouse, 5);
+
+    floor.setNext(backroom);
+    floor.setNext(localStore);
+    floor.setNext(warehouse);
+
+    this.floor = floor;
   }
 
   find(itemName) {
-    var index = this.inventory.map((item) => item.name).indexOf(itemName);
-    return this.inventory[index];
+    return this.Storage.find(itemName);
   }
 }
 
--- Ch04/04_01/start/index.js
+++ Ch04/04_01/start/index.js
@@ -1,7 +1,7 @@
 var Store = require("./Store");
 var inventory = require("./inventory");
 
-var skiShop = new Store("Steep and Deep", inventory.floor);
+var skiShop = new Store("Steep and Deep", inventory);
 
 var searchItem = "ski hats";
 var results = skiShop.find(searchItem);
