cad6f2e29e343a049c1e54f5ef7299fa2b59dc6e
fanweikung <fan-wei.kung@unicodeconsulting.com>
2023-07-04 12:02:42 -0700

    Added and set default to toMorseCode

--- Ch04/04_11/start/LogStrategy.js
+++ Ch04/04_11/start/LogStrategy.js
@@ -1,7 +1,13 @@
 var path = require("path");
 var { appendFile } = require("fs");
+var morse = require("morse");
 
 class LogStrategy {
+  static toMorseCode(timestamp, message) {
+    var morseCode = morse.encode(message);
+    console.log(morseCode);
+  }
+
   static noDate(timestamp, message) {
     console.log(message);
   }
